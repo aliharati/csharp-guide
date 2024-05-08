2ffeb769ec854b46bcb9eb11c36ee3c647438f35
Ali Harati <77158617+aliharati@users.noreply.github.com>
2024-05-08 18:08:24 +0330

    Add ColoredItem generic typed class to wepons

---
+++ Wepons/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ColoredItem<Sword> blueSword = new ColoredItem<Sword>(new Sword(), ConsoleColor.Blue);
+        ColoredItem<Bow> redBow = new ColoredItem<Bow>(new Bow(), ConsoleColor.Red);
+        ColoredItem<Axe> greenAxe = new ColoredItem<Axe>(new Axe(), ConsoleColor.Green);
+
+        blueSword.Display();
+        redBow.Display();
+        greenAxe.Display();
+
+    }
+}
+
+public class Sword { }
+public class Bow { }
+public class Axe { }
+
+public class ColoredItem<T> where T : class 
+{
+    public T Item { get; set; }
+    ConsoleColor Color { get; set; }
+    public ColoredItem(T item, ConsoleColor color)
+    {
+        Item = item;
+        Color = color;
+    }
+
+    public void Display() {
+        Console.ForegroundColor = Color;
+        Console.WriteLine(Item.ToString());
+    }
+
+}
